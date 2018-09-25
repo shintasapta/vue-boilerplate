1d66dccdd075cc68eede805216b2d7469b84bb3b
shinta <saptarinishinta@gmail.com>
2018-09-25 23:17:23 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="full-width center-content">
+    <h1>Hello World!</h1>
+  </div>
+</template>
+<script>
+import HelloComponent from './components/HelloComponent.vue'
+export default {
+  components: {
+    HelloComponent
+  }
+}
+</script>
